LineageOS/android_hardware_mediatek

power-mediatek: Rework implementation for newer perf stack
* MediaTek have made many changes to their perf stack since
  this implementation was initially written, such as implementing
  new hints (DISPLAY_UPDATE_IMMINENT, EXPENSIVE_RENDERING, and even
  SUSTAINED_PERFORMANCE!), and changing the way this Power AIDL
  implementation handled hints.

* Also resolve arbitrary crashes of mtkpower by being more strict
  on what hints we try to send to PowerHAL for handling, only report
  the hints which are strictly supported by the perf stack and reject
  handling unsupported ones (as seen in newer prebuilt
  android.hardware.power-service-mediatek.so).

* We're also dropping support for R perf stack, just kang S+ stack
  from somewhere...

Signed-off-by: bengris32 <bengris32@protonmail.ch>
Change-Id: Iec0957df907d85c1e7228d3dea37d8e6c4adc634

aidl/power-mediatek/Android.mk
@@ -2,21 +2,16 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-# Default to the latest AIDL version
-TARGET_POWERHAL_AIDL_VERSION ?= 2
-
 LOCAL_MODULE := android.hardware.power-service-mediatek
 LOCAL_VENDOR_MODULE := true
 LOCAL_MULTILIB := 64
-LOCAL_VINTF_FRAGMENTS := power-mtk-v$(TARGET_POWERHAL_AIDL_VERSION).xml
+LOCAL_VINTF_FRAGMENTS := power-mtk.xml
 LOCAL_SRC_FILES := Power.cpp
 
 LOCAL_SHARED_LIBRARIES := \
     libbase \
     libbinder_ndk \
-    android.hardware.power-V$(TARGET_POWERHAL_AIDL_VERSION)-ndk
-
-LOCAL_CFLAGS += -DPOWERHAL_AIDL_VERSION=$(TARGET_POWERHAL_AIDL_VERSION)
+    android.hardware.power-V2-ndk
 
 ifneq ($(TARGET_TAP_TO_WAKE_NODE),)
     LOCAL_CFLAGS += -DTAP_TO_WAKE_NODE=\"$(TARGET_TAP_TO_WAKE_NODE)\"

electimon/hardware_mediatek

power/Android.mk
@@ -2,21 +2,16 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-# Default to the latest AIDL version
-TARGET_POWERHAL_AIDL_VERSION ?= 2
-
 LOCAL_MODULE := android.hardware.power-service-mediatek
 LOCAL_VENDOR_MODULE := true
 LOCAL_MULTILIB := 64
-LOCAL_VINTF_FRAGMENTS := power-mtk-v$(TARGET_POWERHAL_AIDL_VERSION).xml
+LOCAL_VINTF_FRAGMENTS := power-mtk.xml
 LOCAL_SRC_FILES := Power.cpp
 
 LOCAL_SHARED_LIBRARIES := \
     libbase \
     libbinder_ndk \
-    android.hardware.power-V$(TARGET_POWERHAL_AIDL_VERSION)-ndk_platform
-
-LOCAL_CFLAGS += -DPOWERHAL_AIDL_VERSION=$(TARGET_POWERHAL_AIDL_VERSION)
+    android.hardware.power-V2-ndk_platform
 
 ifneq ($(TARGET_TAP_TO_WAKE_NODE),)
     LOCAL_CFLAGS += -DTAP_TO_WAKE_NODE=\"$(TARGET_TAP_TO_WAKE_NODE)\"